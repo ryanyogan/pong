b9e32f0b7ea8c0fb8a2ec7ebb4a5a34ec2385b5d
Ryan Yogan <ryanyogan@gmail.com>
2023-12-29 11:18:03 -0600

    feat // a red ball appears in the center and drifts to the right per frame tick

---
+++ src/main.rs
@@ -0,0 +1,69 @@
+use bevy::{prelude::*, sprite::MaterialMesh2dBundle};
+
+const BALL_SIZE: f32 = 5.;
+
+#[derive(Component)]
+struct Position(Vec2);
+
+#[derive(Component)]
+struct Ball;
+
+#[derive(Bundle)]
+struct BallBundle {
+    ball: Ball,
+    position: Position,
+}
+
+impl BallBundle {
+    fn new() -> Self {
+        Self {
+            ball: Ball,
+            position: Position(Vec2::new(0., 0.)),
+        }
+    }
+}
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, (spawn_ball, spawn_camera))
+        .add_systems(Update, (move_ball, project_positions.after(move_ball)))
+        .run();
+}
+
+fn spawn_ball(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+) {
+    let mesh = Mesh::from(shape::Circle::new(BALL_SIZE));
+    let material = ColorMaterial::from(Color::rgb(1., 0., 0.));
+
+    let mesh_handle = meshes.add(mesh);
+    let material_handle = materials.add(material);
+
+    commands.spawn((
+        BallBundle::new(),
+        MaterialMesh2dBundle {
+            mesh: mesh_handle.into(),
+            material: material_handle,
+            ..default()
+        },
+    ));
+}
+
+fn move_ball(mut ball: Query<&mut Position, With<Ball>>) {
+    if let Ok(mut position) = ball.get_single_mut() {
+        position.0.x += 1.0
+    }
+}
+
+fn spawn_camera(mut commands: Commands) {
+    commands.spawn_empty().insert(Camera2dBundle::default());
+}
+
+fn project_positions(mut positionables: Query<(&mut Transform, &Position)>) {
+    for (mut transform, position) in &mut positionables {
+        transform.translation = position.0.extend(0.);
+    }
+}
